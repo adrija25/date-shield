23c2a3f346769afa48f63c1abbcf384dee6c331c
Adrija Choudhury <64792558+adrija25@users.noreply.github.com>
2026-07-19 17:10:13 +0530

    Create content.js

---
+++ content.js
@@ -0,0 +1,5 @@
+// Date Shield
+// Content script for user-triggered profile analysis.
+// Analysis functionality will be added carefully as the MVP is built.
+
+console.log("Date Shield content script loaded.");
